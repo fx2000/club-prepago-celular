83886bb50d89f481b248dd15331760c908aebe52
Jhonmer Araujo <jhonmer.araujo@gmail.com>
2017-05-02 17:01:42 -0500

    Eliminación de PlatfornId en las condiciones de los querires

--- app/webroot/API/UpdateFavorite/Request.UpdateFavorite.php
+++ app/webroot/API/UpdateFavorite/Request.UpdateFavorite.php
@@ -79,7 +79,7 @@ class RequestUpdateFavoriteAPI extends Dbconn {
 		$query = 
 			"SELECT id
 				FROM devices
-				WHERE device_id = " . $deviceId . " AND user_id = " . $userId . " AND platform_id = " . $platformId . " AND login_status = " . SIGNED_IN;
+				WHERE device_id = " . $deviceId . " AND user_id = " . $userId . " AND login_status = " . SIGNED_IN;
 		$result = $this->fireQuery($query);
 		$value = $this->rowCount($result);
 		return $value;
