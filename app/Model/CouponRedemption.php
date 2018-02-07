2ee07c14e23e2195fda5ac41322d6d1e14c79997
Jhonmer Araujo <jhonmer.araujo@gmail.com>
2018-02-06 22:40:48 -0500

    Se crea vista

---
+++ app/Model/CouponRedemption.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Coupon Redeptions model for Club Prepago Celular
+ *
+ * @copyright     Club Prepago Celular(tm) Project
+ * @package       app.Model
+ * @since         Club Prepago Celular(tm) v 1.2
+ */
+class CouponRedemption extends AppModel {
+	public $useTable = 'coupon_redemptions';
+}
